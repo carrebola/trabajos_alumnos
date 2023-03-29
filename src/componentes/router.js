37850a4fa6ec1626e716f20679d42176236368dc
Carlos Arrebola <carrebola@fpllefia.com>
2023-03-29 11:01:21 +0200

    .

--- src/componentes/router.js
+++ src/componentes/router.js
@@ -1,6 +1,6 @@
 import enunciadoDetalleVista from '../vistas/enunciados/detalleEnunciadoVista.js'
 
-// Objeto con todas las rutas y su vista asociada
+// Objeto (diccionario) con todas las rutas y su vista asociada
 const rutas = {
   home: import('../vistas/homeVista.js'),
   // Usuarios
@@ -37,14 +37,16 @@ const router = async () => {
   const parametro = pathCompleto.split('/')[2]
 
   // capturamos el componente con ese nombre de la vista
+  // (Usamos las sintaxix objeto[propiedad]) porque el nombre de la propiedad lo tenemos en una variable.) 
   const componenteVista = await rutas[path]
   // Si existe la vista la podremos cargar
   if (componenteVista) {
     try {
-      // Obtenemos el objeto del componente
+      // Obtenemos el objeto del componente (que fué exportado como default)
       const vista = await componenteVista.default
       // inyectamos vista y ejecutamos su script
       document.querySelector('main').innerHTML = vista.template
+      // A los script les pasamos el parametro que hemos extraido de la ruta. Así podemos pasar, por ejemplo, el id de un proyecto
       vista.script(parametro)
     } catch (error) {
       // Si se produce un error cargamos la vista 404
@@ -55,8 +57,9 @@ const router = async () => {
 
 // Capturamos los eventos
 export const observadorRutas = () => {
-  // Capturamos eventos de los enlaces
+  // Capturamos todas las etiquetas <a> del documento
   const links = document.querySelectorAll('a')
+  // Y añadimos un observador a cada una para detectar click
   links.forEach(link => {
     link.addEventListener('click', event => {
       // Evitamos que se cargue la página
@@ -65,7 +68,7 @@ export const observadorRutas = () => {
       const href = link.getAttribute('href')
       // Añadimos la nueva ruta al historial
 
-      // El método pushState() permite agregar un nuevo estado a la pila del historial del navegador. Esto significa que una nueva entrada de historial se agrega a la pila y la URL del navegador se actualiza sin recargar la página.
+          // (El método pushState() permite agregar un nuevo estado a la pila del historial del navegador. Esto significa que una nueva entrada de historial se agrega a la pila y la URL del navegador se actualiza sin recargar la página.)
       history.pushState({ path: href }, '', href)
       // y ejecutamos el router de nuevo para que detecte los cambios con el evento popstate
       router()
